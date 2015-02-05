d8df0e324b6806d98e51059aa9d719bdec6ecdd8
HKMogul <hilary.mogul@gmail.com>
2015-02-05 14:54:36 -0500

    working click position, next step is fixing array stack

--- spoofProgram/clickSpace.py
+++ spoofProgram/clickSpace.py
@@ -1,43 +1,54 @@
 import Tkinter
-import datetime
+from datetime import datetime
+import numpy as np
 
 
 class simpleapp_tk(Tkinter.Tk):
-    global timerStarted
-    def click(event):
-        print event.x
-        print event.y
+    # hitStack = np.empty([0,4])
     def __init__(self, parent):
         Tkinter.Tk.__init__(self,parent)
         self.parent = parent
         timerStarted = False
+        # global hitStack
+        # hitStack = np.empty([0,4])
+        global startTime
         self.initialize()
+    def click(event, event1):
+        nowTime= datetime.now().microsecond
+        vel = 50
+        print "Position: {0}, {1} \nDelta Time: {2} \n ".format(event1.x, event1.y, (startTime-nowTime))
+        # hitStack = np.vstack((hitStack, np.array([(startTime-nowTime), 50,event1.x, event1.y])))
+        # print hitStack.shape
+
+
 
     def initialize(self):
-        frame = Tkinter.Frame(self, width = 800, height = 600)
+        frame = Tkinter.Frame(self, width = 800, height = 600, bg = "dim gray")
         frame.grid()
-        frame.bind("<Button-1>", click)
-        # global timerStarted
-        global startVar
-        startVar = Tkinter.StringVar().set(u"START")
-        button = Tkinter.Button(self, textvariable = startVar, command = self.hitButton)
-        button.grid(column = 1, row = 0)
+        frame.bind("<Button-1>", self.click)
+        # startTime = datetime.now().microsecond
+        print "Timer started at: {0}".format(startTime)
+        # startVar = Tkinter.StringVar().set(u"START")
+        # button = Tkinter.Button(self, textvariable = startVar, command = self.hitButton)
+        # button.grid(column = 1, row = 0)
 
 
-    def hitButton(event):
-        if not timerStarted:
-            timerStarted = True
-            global startTime
-            startTime= datetime.time()
-            print startTime.isoformat()
-            print "Timer started! Get clickin!"
-            startVar.set("STOP")
-        else:
-            timerStarted = False
-            global stopTime
-            stopTime = datetime.time()
-            startVar.set("START")
+    # def hitButton(event):
+    #     if not timerStarted:
+    #         timerStarted = True
+    #         global startTime
+    #         startTime= datetime.time()
+    #         print startTime.isoformat()
+    #         print "Timer started! Get clickin!"
+    #         startVar.set("STOP")
+    #     else:
+    #         timerStarted = False
+    #         global stopTime
+    #         stopTime = datetime.time()
+    #         startVar.set("START")
 if __name__ == "__main__":
+    global startTime
+    startTime = datetime.now().microsecond
     app = simpleapp_tk(None)
     app.title('Poop Emoji Here')
     app.mainloop()
