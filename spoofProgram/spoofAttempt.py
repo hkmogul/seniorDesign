7d794a73b5f12be3cc51f4ed12d490a562e1e83e
HKMogul <hilary.mogul@gmail.com>
2015-02-05 14:18:31 -0500

    adds more attempts to implement datetime

---
+++ spoofProgram/spoofAttempt.py
@@ -0,0 +1,39 @@
+import Tkinter
+import datetime
+
+
+class simpleapp_tk(Tkinter.Tk):
+		global timerStarted		
+		def __init__(self, parent):
+				Tkinter.Tk.__init__(self,parent)
+				self.parent = parent
+				timerStarted = False
+				self.initialize()
+				
+		def initialize(self):
+				frame = Tkinter.Frame(self, width = 800, height = 600)
+				frame.grid()
+				# global timerStarted 
+				global startVar
+				startVar = Tkinter.StringVar().set("START")	
+				button = Tkinter.Button(self, textvariable = startVar, command = self.hitButton)
+				button.grid(column = 1, row = 0)
+				
+
+		def hitButton(event):
+				if not timerStarted:
+						timerStarted = True
+						global startTime 
+						startTime= datetime.time()
+						print startTime.isoformat()
+						print "Timer started! Get clickin!"
+						startVar.set("STOP")
+				else:
+						timerStarted = False
+						global stopTime 
+						stopTime = datetime.time()
+						startVar.set("START")
+if __name__ == "__main__":
+		app = simpleapp_tk(None)
+		app.title('Poop Emoji Here')
+		app.mainloop()
