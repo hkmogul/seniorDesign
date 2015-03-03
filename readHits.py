e95a5715b47310fadaa4bf651dc8e86b931fdd79
hkmogul <hilary.mogul@gmail.com>
2015-03-03 10:50:01 -0500

    updates, some commenting on todo list

--- readHits.py
+++ readHits.py
@@ -3,18 +3,34 @@
 '''
 
 import numpy as np
+import time
 import datetime
 import serial
 
 ''' Parse string from arduino serial monitor to return location, velocity,
     time, etc
-    Example expected string to parse would be: 'NE056'
+    Example expected string to parse would be: 'NE056'= northeast velocity = 56
     timeStart should be an int in ms declared
 '''
 def parseInput(input = None, timeStart):
     if input is None:
         return None
     # get time first to mitigate delays
-    t =
+    timeDiff = (timeStart - time.time()).
     loc = input[:2]
-    
+
+
+''' Open a serial connection with arduino, then read until some global variable hits
+    Expected to be called upon in a thread, then started/killed when necessary
+'''
+def openComm(address = None, baud = None, hitStack = None):
+    if address is None:
+        ser = serial('/dev/tty.usbmodem1411', 9600)
+    else:
+        ser = serial(address, baud)
+    # TODO: how does this pass values out?, input argument of numpy array?
+    if hitStack is None:
+        # code for print and parse goes here
+    else:
+        global hitStack
+        
