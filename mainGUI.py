8a67ef82a3d5a17cb620313ee600901e28cf4378
hkmogul <hilary.mogul@gmail.com>
2015-03-19 16:36:57 -0400

    most of first frame of option 0 done

--- mainGUI.py
+++ mainGUI.py
@@ -10,6 +10,9 @@ import Tkinter
 import matplotlib.pyplot as plt
 import tkMessageBox
 import tkFileDialog
+import threading
+import time
+
 sys.path.append("libraries")
 # self made libraries
 import readHits
@@ -20,16 +23,21 @@ import config as cfg
 root= Tkinter.Tk()
 root.title("Drum Trainer")
 path = Tkinter.StringVar()
+tempo = Tkinter.IntVar()
+duration = Tkinter.IntVar()
 root.configure(background = 'black')
 def quit():
 	root.quit()
-
+def reset():
+	cfg.resetAll()
 def credits():
 	tkMessageBox.showinfo("Credits", "Team ColDRUMbia 5ever")
 #initial window
 def viewInit():
+	clearOut(root)
+	cfg.resetAll()
 	frameI = Tkinter.Frame(root)
-	frameI.grid()
+	frameI.pack()
 	label = Tkinter.Label(frameI, text = "DRUM TRAINER PROGRAM", relief = Tkinter.RAISED)
 	label.grid(row = 0, column = 1)
 	button0 = Tkinter.Button(frameI, text = "Record new Example", command = option0)
@@ -46,21 +54,22 @@ def viewInit():
 
 	quitButton = Tkinter.Button(frameI, text = "QUIT", command = quit)
 	quitButton.grid(row = 9, column =2)
-	
+
 	creditB = Tkinter.Button(frameI, text = "CREDITS", command = credits)
 	creditB.grid(row = 9, column = 0)
 
+	padWidgets(frameI)
 def option0():
 	clearOut(root)
 	frame0 = Tkinter.Frame(root)
 	frame0.grid()
-	
+
 	nameLabel = Tkinter.Label(frame0, text = "Name of Example/Folder")
 	nameLabel.grid(row = 0,column = 0)
 
 	nameEntry = Tkinter.Entry(frame0, text = "Example")
 	nameEntry.grid(row = 1,column = 0)
-	
+
 
 	global path
 	pathLabel = Tkinter.Label(frame0, text = "Path for Example to be saved")
@@ -72,11 +81,32 @@ def option0():
 	pathButton = Tkinter.Button(frame0, text = "...", command = pathOpen)
 	pathButton.grid(row = 3, column = 1)
 
+	tempoLabel = Tkinter.Label(frame0, text = "TEMPO (bpm)").grid(row = 4, column = 0)
+	#TODO: validation function for numbers only
+	tempoEntry = Tkinter.Entry(frame0, text = 'cfg.tempo').grid(row = 5, column = 0)
+	
+	durationLabel = Tkinter.Label(frame0, text = "Duration (seconds)").grid(row = 6, column = 0)
+	durationEntry = Tkinter.Entry(frame0).grid(row = 7, column = 0)
+
+	homeButton = Tkinter.Button(frame0, text = "Back", command = viewInit).grid(row = 0, column = 5)
+	# TODO: reset fields function outside of this loop?
+	resetButton = Tkinter.Button(frame0, text = "Reset", command = reset).grid(row = 1, column = 5)
+	
+	startButton = Tkinter.Button(frame0, text = "START", command = option0_start, bg = 'GREEN').grid(row = 2, column = 5)
+	padWidgets(frame0)
+	return
+def option0_start():
+	#TODO: function to pick up/set all appropriate values
+	clearOut(root)
+	frame0A = Tkinter.Frame(root).grid()
+	# maybe make a thing that flashes based on tempo in recording adjustment thread? if tempo is set, that is
+def cameraInstructions():
+	tkMessageBox.showinfo("Instructions for Camera Access", "Plug in camera USB hub and select Overhead, Side, and Front video files respectively")
 def pathOpen():
 	path.set(tkFileDialog.askdirectory(parent = root))
 	cfg.userPath = path.get()
 def option1():
-		
+
 	pass
 def option2():
 	pass
@@ -90,6 +120,11 @@ def FUN():
 def clearOut(parent):
 	for widget in parent.winfo_children():
 		widget.destroy()
+
+''' Gives all widgets in a parent some space '''
+def padWidgets(parent):
+	for widget in parent.winfo_children():
+		widget.grid(padx = 5, pady = 2)
 
 viewInit()
 
