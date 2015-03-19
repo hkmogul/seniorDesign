98dae6e0dcfbd90278ffbbfac32551ba6e947da2
hkmogul <hilary.mogul@gmail.com>
2015-03-18 20:55:46 -0400

    successful protocol for getting file path to update

--- mainGUI.py
+++ mainGUI.py
@@ -8,9 +8,8 @@ import sys
 import cv2
 import Tkinter
 import matplotlib.pyplot as plt
-#import tkFont
 import tkMessageBox
-
+import tkFileDialog
 sys.path.append("libraries")
 # self made libraries
 import readHits
@@ -18,21 +17,21 @@ import imageFunctions as img
 import performance_analysis as perf
 import config as cfg
 
-# helv16 = tkFont.Font(family = "Helvetica",size =  16)
 root= Tkinter.Tk()
-# initial opening window
+root.title("Drum Trainer")
+path = Tkinter.StringVar()
+root.configure(background = 'black')
 def quit():
 	root.quit()
 
 def credits():
 	tkMessageBox.showinfo("Credits", "Team ColDRUMbia 5ever")
-
+#initial window
 def viewInit():
 	frameI = Tkinter.Frame(root)
-	frameI.pack()
+	frameI.grid()
 	label = Tkinter.Label(frameI, text = "DRUM TRAINER PROGRAM", relief = Tkinter.RAISED)
 	label.grid(row = 0, column = 1)
-#	global helv16
 	button0 = Tkinter.Button(frameI, text = "Record new Example", command = option0)
 	button0.grid(row = 1, column = 1)
 
@@ -52,8 +51,32 @@ def viewInit():
 	creditB.grid(row = 9, column = 0)
 
 def option0():
-	pass
+	clearOut(root)
+	frame0 = Tkinter.Frame(root)
+	frame0.grid()
+	
+	nameLabel = Tkinter.Label(frame0, text = "Name of Example/Folder")
+	nameLabel.grid(row = 0,column = 0)
+
+	nameEntry = Tkinter.Entry(frame0, text = "Example")
+	nameEntry.grid(row = 1,column = 0)
+	
+
+	global path
+	pathLabel = Tkinter.Label(frame0, text = "Path for Example to be saved")
+	pathLabel.grid(row = 2, column = 0)
+	path.set(cfg.userPath)
+	pathShow = Tkinter.Label(frame0, textvariable = path, relief = Tkinter.SUNKEN)
+	pathShow.grid(row = 3, column = 0)
+
+	pathButton = Tkinter.Button(frame0, text = "...", command = pathOpen)
+	pathButton.grid(row = 3, column = 1)
+
+def pathOpen():
+	path.set(tkFileDialog.askdirectory(parent = root))
+	cfg.userPath = path.get()
 def option1():
+		
 	pass
 def option2():
 	pass
@@ -62,12 +85,6 @@ def option3():
 def FUN():
 	print "AWWW YEEAAAHHHH"
 	pass
-
-
-
-
-
-
 
 
 def clearOut(parent):
