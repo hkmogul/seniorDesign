8851f2a53f7de85f478c604eaca202319b1cb754
Hilary Mogul <hilary.mogul@gmail.com>
2015-03-31 18:20:54 -0400

    sets color thresholds

--- libraries/config.py
+++ libraries/config.py
@@ -3,7 +3,7 @@
 '''
 import numpy as np
 import os
-
+import scipy.io
 # if in recording mode
 recording = False
 
@@ -31,21 +31,28 @@ gtAngles = np.empty((2,0))
 
 '''color settings (HSV)'''
 # lower and upper bounds for tip of Right stick
-tipR_lower = np.array([85,140,175])
-tipR_upper = np.array([95,200,255])
+# tipR_lower = np.array([170,100,75])
+# tipR_upper = np.array([179,255,255])
+# BRIGHT YELLOW GAFF
+# orig hsv 42, 192, 198
+tipR_lower = np.array([35,75,150])
+tipR_upper = np.array([45,255,255])
+
 
 #lower and upper bounds for band of Right stick
-stickR_lower = np.array([0,0,0])
-stickR_upper = np.array([0,0,0])
+# GREEN GAFF- 69, 165, 191
+stickR_lower = np.array([65,100,150])
+stickR_upper = np.array([80,255,255])
 
 
 # lower and upper bounds for tip of Left stick
-tipL_lower = np.array([7,175,175])
-tipL_upper = np.array([15,255,255])
-
+# PINK- 161,164,255
+tipL_lower = np.array([155,60,90])
+tipL_upper = np.array([166,255,255])
 #lower and upper bounds for band of Left stick
-stickL_lower = np.array([0,0,0])
-stickL_upper = np.array([0,0,0])
+# ORANGE - 2, 184, 252
+stickL_lower = np.array([0,155,155])
+stickL_upper = np.array([8,255,255])
 
 ptsThresh = 250
 ''' paths to folders for each '''
@@ -53,9 +60,8 @@ userPath = ''
 #if not os.path.exists(userPath):
  #   os.mkdir(userPath)
 userFolder = ''
-
+''' GT PATH SHOULD ALSO INCLUDE THE FOLDER '''
 gtPath = ''
-gtFolder = ''
 #if not os.path.exists(gtPath):
  #   os.mkdir(gtPath)
 
@@ -64,6 +70,18 @@ unoPath = ''
 megaPath = ''
 unoBaud = 9600
 megaBaud = 9600
+''' loadmat of values from gt.  returns true if successful '''
+def loadGT():
+    if os.path.isfile(os.path.join(gtPath, 'data.mat')):
+        mat_contents = scipy.io.loadmat(os.path.join(gtPath, 'data.mat'))
+        gtHeights = mat_contents['heights']
+        gtAngles = mat_contents['angles']
+        groundTruth = mat_contents['hits']
+        tempo = mat_contents['tempo']
+        duration = mat_contents['duration']
+        return True 
+    else:
+        return False
 def setUserPath(name):
     userPath = name
     if not os.path.exists(userPath):
@@ -74,10 +92,7 @@ def setUserFolder(name):
     if not os.path.exists(os.path.join(userPath, userFolder)):
         os.mkdir(os.path.join(userPath, userFolder))
 
-def setGTFolder(name):
-    gtFolder = name
-    if not os.path.exists(os.path.join(gtPath, gtFolder)):
-        os.mkdir(os.path.join(gtPath, gtFolder))
+
 ''' Check if a folder path already exists '''
 def checkFolderExists(name):
     pass
@@ -126,12 +141,11 @@ def resetAll():
     stickL_upper = np.array([0,0,0])
     ptsThresh = 250
     ''' paths to folders for each '''
-    userPath = '~/Desktop/UserInfo'
+    userPath = ''
     #if not os.path.exists(userPath):
      #   os.mkdir(userPath)
-    userFolder = '/'
+    userFolder = ''
 
-    gtPath = '~/Desktop/TeacherInfo'
-    gtFolder = '/'
+    gtPath = ''
     #if not os.path.exists(gtPath):
      #   os.mkdir(gtPath)
