b31679e997a12faf4c13c1bdb712dc43518a871b
Hilary Mogul <hilary.mogul@gmail.com>
2015-04-07 11:18:09 -0400

    more commenting and got rid of useless variables

--- sensorCode/sensorCode.ino
+++ sensorCode/sensorCode.ino
@@ -1,14 +1,7 @@
-int sensor0 = A0;
-int sensor1 = A1;
-int sensor2 = A2;
-int sensor3 = A3;
-int sensor4 = A4;
-int sensor5 = A5;
-int sensor6 = A6;
-int sensor7 = A7;
-int sensor8 = A8;
-
-//const static int sensePins[9]={sensor0, sensor1, sensor2, sensor3, sensor4, sensor5, sensor6, sensor7, sensor8};
+/*
+New and improved code for Team ColDRUMbia position sensing drum pad
+Hilary Mogul & Shahrod Khalkali
+*/
 const static int sensePins[9]={A0, A1, A2, A3, A4, A5, A6, A7, A8};
 
 const static int xPositions[9] = {-1,0,1,-1,0,1,-1,0,1};
@@ -18,11 +11,12 @@ static int prevValues[9] = {0,0,0,0,0,0,0,0,0};
 static int vel;
 float elapsedTime = 1;
 // Thresholds go here
-// TODO: can we do analog read 2x in a row for a faster runtime and still 
-// get an accurate slope reading?
+
+
+
 // possibly check for below threshold && local minima to make sure we 
 /*
-If hit detected, return 1-127 velocity, 0 for non hit
+If hit detected, return 1-127 velocity, 0 for non hit- dont forget to load
 */
 int hitCheck(int pin, int prev){
   // DETECTION ALGORITHM GOES HERE
